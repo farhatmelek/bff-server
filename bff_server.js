26566cbfa6f60a971496b1e7516749253e024e92
Melek FARHAT <melek.farhat@etu.univ-cotedazur.fr>
2024-09-20 19:47:00 +0200

    first commit

---
+++ bff_server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/menu', async (req, res) => {
+  try {
+    const type = req.query.type;  // Récupérer le type (category) depuis les paramètres de la requête
+    const response = await axios.get('http://localhost:9500/menu/menus');  // Récupérer les données du back-end
+    
+    // Supposons que le back-end renvoie une liste d'items
+    const menuItems = response.data;
+
+    // Filtrer les items selon la catégorie (type)
+    const filteredItems = menuItems.filter(item => item.category.toLowerCase() === type.toLowerCase());
+
+    // Renvoyer les items filtrés au front-end
+    res.status(200).json(filteredItems);
+  } catch (error) {
+    console.error('Erreur lors de la requête au back-end:', error);
+    res.status(500).json({ message: 'Erreur interne du serveur' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Le serveur BFF écoute sur le port ${PORT}`);
+});
