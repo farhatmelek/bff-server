a3c0480060a297943c8304d67c342d054a6aae43
Yasmine Moussaoui <yasminemoussaoui@MacBook-Air-de-Yasmine.local>
2024-09-30 10:49:13 +0200

    ajouter methode create order

--- bff_server.js
+++ bff_server.js
@@ -27,7 +27,53 @@ app.get('/menu', async (req, res) => {
     res.status(500).json({ message: 'Erreur interne du serveur' });
   }
 });
+
+// Route POST /tableOrders
+app.post('/tableOrders', async (req, res) => {
+  try {
+    // Récupérer les données du body de la requête (tableNumber et customersCount)
+    const { tableNumber, customersCount } = req.body;
+    console.log("createTableOrder")
+    console.log(tableNumber);
+    console.log(customersCount);
+
+    // Vérification basique des données
+    if (typeof tableNumber !== 'number' || typeof customersCount !== 'number') {
+      return res.status(400).json({ message: 'Invalid data format. Expected numbers.' });
+    }
+
+    // Envoyer une requête POST à une API externe (par exemple : 'http://localhost:9500/tableOrders')
+    const response = await axios.post('http://localhost:9500/dining/tableOrders', {
+      tableNumber,
+      customersCount
+    });
+
+    // Si la requête réussit, renvoyer la réponse de l'API externe au client
+    res.status(201).json({
+      message: 'Table order sent to the external API successfully',
+      data: response.data
+    });
+
+  } catch (error) {
+    console.error('Erreur lors de la requête à l\'API externe:', error);
+
+    // Gérer les erreurs en renvoyant une réponse appropriée au client
+    if (error.response) {
+      // Erreur venant de l'API externe
+      res.status(error.response.status).json({
+        message: 'Erreur lors de la communication avec l\'API externe',
+        error: error.response.data
+      });
+    } else {
+      // Erreur côté serveur ou autre
+      res.status(500).json({ message: 'Erreur interne du serveur' });
+    }
+  }
+});
+
+
 app.use('/dining',diningRoutes);
 app.listen(PORT, () => {
   console.log(`Le serveur BFF écoute sur le port ${PORT}`);
 });
+
